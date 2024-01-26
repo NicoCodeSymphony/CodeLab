96d494c6926aac7528b43ee9a3059831a2b7fd8d
John Nico M. Edisan <nicxsician@gmail.com>
2024-01-26 11:06:38 +0800

    Create sampleQuery.sql

---
+++ sampleQuery.sql
@@ -0,0 +1,31 @@
+-- CREATE DATABASE nico;
+
+use finalflutter;
+
+-- CREATE TABLE course(
+-- 	course_id INT(11) NOT NULL AUTO_INCREMENT,
+--     course_name VARCHAR(45) NOT NULL,
+--     department_id INT(11) NOT NULL,
+--     PRIMARY KEY (course_id)
+-- );
+
+-- SELECT * FROM customers where Country IN('Mexico', 'Spain');
+-- SELECT * FROM customers where country like 'A%';
+-- SELECT * FROM customers ORDER BY ContactName DESC;
+
+
+-- CREATE TABLE sample(
+-- 	ID INT(11) NOT NULL AUTO_INCREMENT,
+--     name VARCHAR(50) NOT NULL,
+--     PRIMARY KEY (ID)
+-- );
+
+-- ALTER TABLE sample
+-- ADD address VARCHAR(100) NOT NULL 
+
+-- INSERT INTO sample (name, address) VALUES ('John Nico Edisan', 'Bartolome Hills Bungtod, Bogo City, Cebu');
+
+-- UPDATE sample SET name = "IZZY" WHERE ID = 2; 
+
+-- DELETE FROM sample WHERE ID = 2;
+
